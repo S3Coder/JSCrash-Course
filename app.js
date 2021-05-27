e64528196e93249673db6cba0503b20381552a57
Sibil Sarjam Soren <84779489+S3Coder@users.noreply.github.com>
2021-05-27 14:42:38 +0530

    New functions added

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log("Start");
+
+var n = 2;
+function square (num){
+    var ans = num * num;
+    return ans;
+}
+
+var sq2 = square(n);
+var sq4 = square(4);
+
+console.log("End");
+
+console.log(sq2,sq4);
